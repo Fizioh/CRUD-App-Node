2ed83040f06c49b0f6332661e0f1e7accec0bce7
Benosmane Yassine <54205753+Fizioh@users.noreply.github.com>
2020-10-12 10:47:33 +0200

    import + json body-parser

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
