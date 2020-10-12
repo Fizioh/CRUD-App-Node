c060d8178892a9bde7b36b429c589dfa211c9c90
Benosmane Yassine <54205753+Fizioh@users.noreply.github.com>
2020-10-12 10:46:02 +0200

    ajout middleware route get

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,24 @@
 const express = require('express');
+const app = express();
 
-const app = expres();
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+app.get('/api/products', (req, res, next) => {
+    const product = [
+        {
+            name: 'Chapeau',
+            description: 'Chapeau en soie',
+            price: 28,
+            inStock: true
+        }
+    ];
+    next();
+})
+
 
 module.exports = app;
