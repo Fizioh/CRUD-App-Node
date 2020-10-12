f05835248596e5d9fb9f8232786ec34828b17424
Benosmane Yassine <54205753+Fizioh@users.noreply.github.com>
2020-10-12 14:42:23 +0200

    instanciation modele thing

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,18 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const Thing = require('./models/thing');
 
-app.use(bodyParser.json());
+mongoose.connect('mongodb+srv://Eni:Nagato59500@eni.vvqvt.gcp.mongodb.net/ENI?retryWrites=true&w=majority',
+{ useNewUrlParser: true,
+    useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'))
+
+
+const app = express();
+
+
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -11,12 +21,25 @@ app.use((req, res, next) => {
     next();
   });
 
+  app.use(bodyParser.json());
+
+
+  app.post('/api/products', (req, res, next) => {
+     delete req.body._id;
+     const thing = new Things({
+         ...req.body
+     });
+     thing.save()
+     .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+     .catch(error => res.status(400).json({ error }));
+  });
+
 app.get('/api/products', (req, res, next) => {
     const product = [
         {
-            name: 'Chapeau',
-            description: 'Chapeau en soie',
-            price: 28,
+            name: 'Mac',
+            description: 'Apple',
+            price: 2800,
             inStock: true
         }
     ];
